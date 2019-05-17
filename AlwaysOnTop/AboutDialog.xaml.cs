e259bef0fd1fa7425b89290b4049477b2cd7aa69
ayamadori <ayamadori@gmail.com>
2019-05-17 23:58:43 +0900

    Add feedback button, update review button

--- AlwaysOnTop/AboutDialog.xaml.cs
+++ AlwaysOnTop/AboutDialog.xaml.cs
@@ -12,6 +12,12 @@ namespace AlwaysOnTop
         public AboutDialog()
         {
             this.InitializeComponent();
+
+            // https://docs.microsoft.com/en-us/windows/uwp/monetize/launch-feedback-hub-from-your-app
+            if (Microsoft.Services.Store.Engagement.StoreServicesFeedbackLauncher.IsSupported())
+            {
+                this.FeedbackButton.Visibility = Visibility.Visible;
+            }
         }
 
         private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
@@ -21,8 +27,15 @@ namespace AlwaysOnTop
         private async void RateButton_Click(object sender, RoutedEventArgs e)
         {
             // https://github.com/Microsoft/Windows-task-snippets/blob/master/tasks/Store-app-rating-pop-up.md
-            var uriReview = new Uri($"ms-windows-store://REVIEW?PFN={Package.Current.Id.FamilyName}");
+            var uriReview = new Uri($"ms-windows-store://review/?ProductId=9p37ft08rnxm");
             var success = await Windows.System.Launcher.LaunchUriAsync(uriReview);
+        }
+
+        private async void FeedbackButton_Click(object sender, RoutedEventArgs e)
+        {
+            // https://docs.microsoft.com/en-us/windows/uwp/monetize/launch-feedback-hub-from-your-app
+            var launcher = Microsoft.Services.Store.Engagement.StoreServicesFeedbackLauncher.GetDefault();
+            await launcher.LaunchAsync();
         }
     }
 }
