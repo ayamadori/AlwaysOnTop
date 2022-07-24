d1d603cc38eff7278c4ee20d5207db1df5be6abd
ayamadori <ayamadori@gmail.com>
2022-07-24 14:35:07 +0900

    Bump version to 2.0.0

--- AlwaysOnTop/Properties/AssemblyInfo.cs
+++ AlwaysOnTop/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Always on Top")]
-[assembly: AssemblyCopyright("Copyright © 2020 Ayamadori")]
+[assembly: AssemblyCopyright("Copyright © 2022 Ayamadori")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: ComVisible(false)]
